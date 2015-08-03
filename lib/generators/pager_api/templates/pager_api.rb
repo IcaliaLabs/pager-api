1acafb8d284777481f072ab97517affe20a74919
Abraham Kuri <kurenn@icalialabs.com>
2015-08-03 16:29:38 -0500

    Adds missing config variables for the pager

--- lib/generators/pager_api/templates/pager_api.rb
+++ lib/generators/pager_api/templates/pager_api.rb
@@ -1,12 +1,21 @@
 # Use this module to configure pager the available options
 #
+# Made with love by @icalialabs
 
 PagerApi.setup do |config|
 
   # Pagination Handler
-  # User this option to meet your pagination handler, wheter is :kaminari or :will_paginate
+  # User this option to meet your pagination handler, whether is :kaminari or :will_paginate
   # config.pagination_handler = :kaminari
 
   # Includes Pagination information on Meta
-  # config.include_pagination_on_meta = false
+  #
+  # config.include_pagination_on_meta = true
+
+  # Includes Pagination information on a Link Header
+  #
+  # config.include_pagination_headers = false
+
+  # Set the Total-Count Header name
+  # config.total_count_header = "X-Total-Count"
 end
