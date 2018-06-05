c56e064e843f04dccf87f99c728079a7d90ff2f8
Abraham Kuri <kurenn@icalialabs.com>
2018-06-05 13:37:35 -0500

    Removes empty space at the end of line

--- lib/generators/pager_api/install_generator.rb
+++ lib/generators/pager_api/install_generator.rb
@@ -1,7 +1,7 @@
 module PagerApi
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../templates", __FILE__) 
+      source_root File.expand_path("../templates", __FILE__)
 
       desc "Creates a PagerApi initializer in your application"
 
