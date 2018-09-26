09af888962428e73ee4b23c07f394bbe9f7801ac
Abraham Kuri <kurenn@icalialabs.com>
2018-09-26 16:11:06 -0500

    Gives a bit of style to the page and hooks files

--- lib/pager_api/hooks.rb
+++ lib/pager_api/hooks.rb
@@ -1,9 +1,9 @@
+begin; require "kaminari"; rescue LoadError; end
+begin; require "will_paginate"; rescue LoadError; end
+begin; require "pagy"; rescue LoadError; end
+
 # Dynamic pagination handler call
 require "pager_api/pagination/#{PagerApi.pagination_handler}"
-
-begin; require 'kaminari';           rescue LoadError; end
-begin; require 'will_paginate';      rescue LoadError; end
-begin; require 'pagy';               rescue LoadError; end
 
 if defined?(ActionController::Base)
   ActionController::Base.send(:include, "PagerApi::Pagination::#{PagerApi.pagination_handler.to_s.classify}".constantize)
@@ -12,7 +12,7 @@ else
 end
 
 unless defined?(Kaminari) or defined?(WillPaginate) or defined?(Pagy)
-  Kernel.warn <<-WARNING.gsub(/^\s{4}/, '')
+  Kernel.warn <<-WARNING.gsub(/^\s{4}/, "")
     Warning: pager-api needs Kaminari, Will Paginate or Pagy as a dependency.
     You need to add it to your Gemfile
 
--- lib/pager_api/pagination/pagy.rb
+++ lib/pager_api/pagination/pagy.rb
@@ -1,6 +1,8 @@
 module PagerApi
   module Pagination
     module Pagy
+      include ::Pagy::Backend
+
       def paginate(*args)
         options = args.extract_options!
         collection = args.first
@@ -18,70 +20,68 @@ module PagerApi
 
       private
 
-        # Link: <http://example.com/api/v1/users?page="2">; rel="next",
-        # <http://example.com/api/v1//users?page="5">; rel="last",
-        # <http://example.com/api/v1//users?page="1">; rel="first",
-        # <http://example.com/api/v1/users?page="1">; rel="prev",
-        def pagination_headers(pagy)
-          links = (headers['Link'] || "").split(',').map(&:strip)
-          clean_url = request.original_url.sub(/\?.*$/, '')
+      # Link: <http://example.com/api/v1/users?page="2">; rel="next",
+      # <http://example.com/api/v1//users?page="5">; rel="last",
+      # <http://example.com/api/v1//users?page="1">; rel="first",
+      # <http://example.com/api/v1/users?page="1">; rel="prev",
+      def pagination_headers(pagy)
+        links = (headers["Link"] || "").split(",").map(&:strip)
+        clean_url = request.original_url.sub(/\?.*$/, "")
 
-          paging_info = pages(pagy)
+        paging_info = pages(pagy)
 
-          paging_info.each do |key, value|
-            query_params = request.query_parameters.merge(page: value)
-            links << %Q{ <#{clean_url}?#{query_params.to_param}>; rel="#{key}" }
-          end
-
-          headers['Link'] = links.join(", ") unless links.empty?
-          headers[PagerApi.total_pages_header] = pagy.pages
-          headers[PagerApi.total_count_header] = pagy.count
-
-          return nil
+        paging_info.each do |key, value|
+          query_params = request.query_parameters.merge(page: value)
+          links << %Q{ <#{clean_url}?#{query_params.to_param}>; rel="#{key}" }
         end
 
-        def pagination_links(pagy)
-          current_uri = request.env['PATH_INFO']
-          meta_links = {}
+        headers["Link"] = links.join(", ") unless links.empty?
+        headers[PagerApi.total_pages_header] = pagy.pages
+        headers[PagerApi.total_count_header] = pagy.count
 
-          pages(pagy).each do |key, value|
-            query_params = request.query_parameters.merge(page: value)
-            meta_links[key] = "#{current_uri}?#{query_params.to_param}"
-          end
+        return nil
+      end
 
-          meta_links
+      def pagination_links(pagy)
+        current_uri = request.env["PATH_INFO"]
+        meta_links = {}
+
+        pages(pagy).each do |key, value|
+          query_params = request.query_parameters.merge(page: value)
+          meta_links[key] = "#{current_uri}?#{query_params.to_param}"
         end
 
-        def pages(pagy)
-          {}.tap do |paging|
-            paging[:first] = 1
-            paging[:last] = pagy.pages
+        meta_links
+      end
 
-            paging[:prev] = pagy.prev unless pagy.prev.nil?
-            paging[:next] = pagy.next unless pagy.next.nil?
-          end
+      def pages(pagy)
+        {}.tap do |paging|
+          paging[:first] = 1
+          paging[:last] = pagy.pages
+
+          paging[:prev] = pagy.prev unless pagy.prev.nil?
+          paging[:next] = pagy.next unless pagy.next.nil?
         end
+      end
 
-        def paginate_collection(collection, options = {})
-          options[:page] = params[:page] || 1
-          options[:items] = options.delete(:per_page) || params[:per_page] || ::Pagy::VARS[:items]
+      def paginate_collection(collection, options = {})
+        options[:page] = params[:page] || 1
+        options[:items] = options.delete(:per_page) || params[:per_page] || ::Pagy::VARS[:items]
 
-          meta, collection = pagy(collection, options)
-          [meta, collection]
-        end
+        meta, collection = pagy(collection, options)
+        [meta, collection]
+      end
 
-        def meta(pagy, options = {})
-          {
-            pagination:
-            {
-              per_page: pagy.items,
-              total_pages: pagy.pages,
-              total_objects: pagy.count,
-              links: pagination_links(pagy)
-            }
-          }
-        end
-
+      def meta(pagy, options = {})
+        {
+          pagination: {
+            per_page: pagy.items,
+            total_pages: pagy.pages,
+            total_objects: pagy.count,
+            links: pagination_links(pagy),
+          },
+        }
+      end
     end
   end
 end
