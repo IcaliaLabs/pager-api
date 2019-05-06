309e76f0812d08837013c2b5f299146e35399802
Sergey Elushev <root@grandman73.ru>
2019-05-06 17:57:46 +0300

    require only one pagination gem (#32)

--- lib/pager_api/hooks.rb
+++ lib/pager_api/hooks.rb
@@ -1,21 +1,23 @@
-begin; require "kaminari"; rescue LoadError; end
-begin; require "will_paginate"; rescue LoadError; end
-begin; require "pagy"; rescue LoadError; end
+pagination_handler_name = PagerApi.pagination_handler.to_s
 
-# Dynamic pagination handler call
-require "pager_api/pagination/#{PagerApi.pagination_handler}"
-
-if defined?(ActionController::Base)
-  ActionController::Base.send(:include, "PagerApi::Pagination::#{PagerApi.pagination_handler.to_s.classify}".constantize)
-else
-  ActionController::API.send(:include, "PagerApi::Pagination::#{PagerApi.pagination_handler.to_s.classify}".constantize)
-end
-
-unless defined?(Kaminari) or defined?(WillPaginate) or defined?(Pagy)
+begin
+  require pagination_handler_name
+rescue LoadError
   Kernel.warn <<-WARNING.gsub(/^\s{4}/, "")
-    Warning: pager-api needs Kaminari, Will Paginate or Pagy as a dependency.
+    Warning: pager-api needs #{pagination_handler_name} as a dependency.
     You need to add it to your Gemfile
 
-    gem 'kaminari', gem 'will_paginate' or gem 'pagy'
+    gem '#{pagination_handler_name}'
+
+    Also you can change pagination_handler in pager_api initializer
   WARNING
+end
+
+# Dynamic pagination handler call
+require "pager_api/pagination/#{pagination_handler_name}"
+
+if defined?(ActionController::Base)
+  ActionController::Base.send(:include, "PagerApi::Pagination::#{pagination_handler_name.classify}".constantize)
+else
+  ActionController::API.send(:include, "PagerApi::Pagination::#{pagination_handler_name.classify}".constantize)
 end
